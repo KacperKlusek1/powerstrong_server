4bbe660c15d51c27656c1039301b0d91abe89ff1
KacperKlusek1 <klusekkacper4@gmail.com>
2025-10-29 19:32:30 +0100

    Feat: Initialized Project

---
+++ src/main/java/com/example/projekt_serwer/ProjektSerwerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_serwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSerwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektSerwerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt_serwer/ProjektSerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_serwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSerwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
