9d919125dfd1bc5089a6d1dba2fdb0bbc8b5345d
KacperKlusek1 <132588418+KacperKlusek1@users.noreply.github.com>
2025-11-26 14:07:39 +0100

    Feat: Init Project (#POWERSTRONG)

---
+++ src/test/java/pl/pollub/powerstrong_server/ProjektSerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pollub.powerstrong_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSerwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
